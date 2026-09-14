d216dd8faa5c1714e24a248a387f7e4775cbf545
Nicolas <nicoldiaz@uade.edu.ar>
2026-09-14 17:40:12 -0300

    fix(app): remove accidentally pasted error text inside build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,19 +24,7 @@ android {
     }
 
     buildTypes {
-        release {class com.android.build.gradle.internal.dsl.ApplicationExtensionImpl$AgpDecorated_Decorated cannot be cast to class com.android.build.gradle.BaseExtension (com.android.build.gradle.internal.dsl.ApplicationExtensionImpl$AgpDecorated_Decorated and com.android.build.gradle.BaseExtension are in unnamed module of loader org.gradle.internal.classloader.VisitableURLClassLoader$InstrumentingVisitableURLClassLoader @36a58f4e)
-            class com.android.build.gradle.internal.dsl.ApplicationExtensionImpl$AgpDecorated_Decorated cannot be cast to class com.android.build.gradle.BaseExtension (com.android.build.gradle.internal.dsl.ApplicationExtensionImpl$AgpDecorated_Decorated and com.android.build.gradle.BaseExtension are in unnamed module of loader org.gradle.internal.classloader.VisitableURLClassLoader$InstrumentingVisitableURLClassLoader @36a58f4e)
-
-            Gradle's dependency cache may be corrupt (this sometimes occurs after a network connection timeout.)
-
-            Re-download dependencies and sync project (requires network)
-            The state of a Gradle build process (daemon) may be corrupt. Stopping all Gradle daemons may solve this problem.
-
-            Stop Gradle build processes (requires restart)
-            Your project may be using a third-party plugin which is not compatible with the other plugins in the project or the version of Gradle requested by the project.
-
-            In the case of corrupt Gradle processes, you can also try closing the IDE and then killing all Java processes.
-
+        release {
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
